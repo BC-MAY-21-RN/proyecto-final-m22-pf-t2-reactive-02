3ee00571a57acbefb77fc79885245e631c71aab3
Samuel <samuel16mendoza@gmail.com>
2022-07-08 18:16:59 -0500

    homeScreen was created
    homeScreen was created with navigation for can use it.

--- petmyheart/src/navigation/index.js
+++ petmyheart/src/navigation/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import registerScreen from '../screens/registerScreen';
+import homeScreen from '../screens/homeScreen';
 
 const Stack = createNativeStackNavigator();
+
 const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="registerScreen" component={registerScreen} />
+        <Stack.Screen name="Home" component={homeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ petmyheart/src/screens/homeScreen/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function homeScreen() {
+  return (
+    <View>
+      <Text>Hola</Text>
+    </View>
+  );
+}
