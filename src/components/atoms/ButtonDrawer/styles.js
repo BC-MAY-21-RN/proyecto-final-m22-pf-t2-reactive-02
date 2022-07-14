6a69d1750d66297d0376fa6b03a76169030436ce
Samuel <samuel16mendoza@gmail.com>
2022-07-14 03:30:05 -0500

    ButtonDrawer component was created
    This component needs a text, icon and navigation props to view on the drawer.

---
+++ src/components/atoms/ButtonDrawer/styles.js
@@ -0,0 +1,29 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  iconPosition: {
+    color: '#717171',
+    size: 20,
+    marginRight: 10,
+    backgroundColork: 'red',
+  },
+  container: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: 80,
+  },
+  containerItem: {
+    flexDirection: 'row',
+    flex: 1,
+    marginTop: 25,
+    borderBottomWidth: 1,
+    borderColor: '#E8EAF1',
+    paddingLeft: 25,
+  },
+  text: {
+    marginTop: 1,
+    fontSize: 16,
+  },
+});
+
+export default styles;
