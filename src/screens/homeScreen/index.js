c7b2b5ddb405fececead24b3d5bdb430a4f7e55d
LizyMiGo <jmiguel@ucol.mx>
2022-07-18 17:28:01 -0500

    Post component added to home screen

--- src/screens/homeScreen/index.js
+++ src/screens/homeScreen/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import auth from '@react-native-firebase/auth';
+import Post from '../../components/molecules/Post';
 
 const logout = () => {
   auth()
@@ -12,6 +13,7 @@ export default function HomeScreen() {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Post></Post>
       <TouchableOpacity
         onPress={() => {
           logout();
