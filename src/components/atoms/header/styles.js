4b2d742b7994579427d13f78d253387ade912609
Samuel <samuel16mendoza@gmail.com>
2022-07-22 18:26:32 -0500

    Header component was created
    This component allow us go back to the previous view and visualize
    the name of the current view.

---
+++ src/components/atoms/header/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    height: 60,
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginVertical: 15,
+    color: '#263238',
+  },
+  icon: {
+    marginHorizontal: 15,
+    marginTop: 19,
+  },
+});
+export default styles;
