cbec2d7746038e3931dd74b4ffe5410af184fda1
LizyMiGo <jmiguel@ucol.mx>
2022-07-29 17:20:34 -0500

    Modified component to avoid repetitions
    The component was modified into a smaller one

--- src/components/atoms/UserProfileInfo/index.js
+++ src/components/atoms/UserProfileInfo/index.js
@@ -2,20 +2,12 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
 
-export default function UserProfileInfo({publications, likes, saved}) {
+export default function UserProfileInfo({info, title}) {
   return (
     <View style={styles.container}>
       <View style={styles.space}>
-        <Text style={styles.info}>{publications}</Text>
-        <Text style={styles.title}>Publicaciones</Text>
-      </View>
-      <View style={styles.space}>
-        <Text style={styles.info}>{likes}</Text>
-        <Text style={styles.title}>Me gusta</Text>
-      </View>
-      <View>
-        <Text style={styles.info}>{saved}</Text>
-        <Text style={styles.title}>Guardados</Text>
+        <Text style={styles.info}>{info}</Text>
+        <Text style={styles.title}>{title}</Text>
       </View>
     </View>
   );
