0aed5bdb9e005adcb9e98b371414e8c4ca2eca04
unknown <dag_98@hotmail.com>
2022-07-29 16:46:16 -0500

    add imageviewer

--- src/components/atoms/ImageUpload/index.js
+++ src/components/atoms/ImageUpload/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import {View, TouchableOpacity, Alert} from 'react-native';
+import React, {useState} from 'react';
+import {View, TouchableOpacity, Alert, ScrollView, Modal} from 'react-native';
 import * as ImagePicker from 'react-native-image-picker';
 import {request, PERMISSIONS, check, RESULTS} from 'react-native-permissions';
+import ImageViewer from 'react-native-image-zoom-viewer';
 import {Icon, Avatar} from 'react-native-elements';
 import styles from './styles';
 
-const addImage = () => {
-  ImagePicker.launchImageLibrary();
+const addImage = async (change, post) => {
+  const result = await ImagePicker.launchImageLibrary();
+  if (result.didCancel === true) {
+    Alert.alert('Se cerro la galería', '', [{text: 'OK'}]);
+  } else {
+    const array = [...post.valuesPost.images, result.assets[0].uri];
+    change(array, 'images');
+  }
 };
 
 const launchCamera = async (change, post) => {
@@ -46,17 +53,35 @@ const getPermissions = (change, post) => {
 };
 
 export default function ImageUpload({change, post}) {
+  const [imageOpen, setImageOpen] = useState(false);
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={addImage} style={styles.button}>
-        <Icon name={'image'} type={'feather'} size={30} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => getPermissions(change, post)}>
-        <Icon name={'camera'} type={'feather'} size={30} />
-      </TouchableOpacity>
-      {post.valuesPost.images.map((image, index) => (
-        <Avatar key={index} source={{uri: image}} />
-      ))}
+    <View>
+      <View style={styles().container}>
+        <TouchableOpacity
+          onPress={() => addImage(change, post)}
+          style={styles().button}>
+          <Icon name={'image'} type={'feather'} size={30} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => getPermissions(change, post)}>
+          <Icon name={'camera'} type={'feather'} size={30} />
+        </TouchableOpacity>
+      </View>
+      <ScrollView
+        horizontal={true}
+        style={styles(post.valuesPost.images.length > 0 ? -75 : 0).scrollimage}>
+        {post.valuesPost.images.map((image, index) => (
+          <Avatar
+            key={index}
+            source={{uri: image}}
+            size={80}
+            containerStyle={{marginRight: 10}}
+            onPress={() => setImageOpen(true)}
+          />
+        ))}
+      </ScrollView>
+      <Modal visible={imageOpen} transparent={true}>
+        <ImageViewer imageUrls={post.valuesPost.images} />
+      </Modal>
     </View>
   );
 }
--- src/components/atoms/ImageUpload/styles.js
+++ src/components/atoms/ImageUpload/styles.js
@@ -1,13 +1,20 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    marginTop: 30,
-  },
-  button: {
-    marginHorizontal: 20,
-  },
-});
+const styles = value =>
+  StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      marginTop: 30,
+    },
+    button: {
+      marginHorizontal: 20,
+    },
+    scrollimage: {
+      flexDirection: 'row',
+      marginBottom: value,
+      marginTop: 10,
+      marginLeft: 20,
+    },
+  });
 
 export default styles;
