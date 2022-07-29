b3f740ca189fb2bca0a9492540e3e985ce5a75dc
unknown <dag_98@hotmail.com>
2022-07-29 17:04:34 -0500

    add mock

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -7,6 +7,7 @@ import React from 'react';
 import App from '../App';
 import '@react-native-google-signin/google-signin';
 import 'react-native-gesture-handler/jestSetup';
+import 'react-native-permissions';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -15,6 +16,9 @@ jest.useFakeTimers();
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
 jest.mock('@react-native-google-signin/google-signin', () => {});
+jest.mock('react-native-permissions', () => {
+  require('react-native-permissions/mock');
+});
 
 it('renders correctly', () => {
   renderer.create(<App />);
