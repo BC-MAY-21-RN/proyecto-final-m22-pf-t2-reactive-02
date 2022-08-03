2ab8a305be167a81751b7436d607ca0f3d8ce5ac
mrivh <m.alicia.riv@gmail.com>
2022-08-03 03:42:51 -0500

    Fix: styles

--- src/components/atoms/ButtonForgetScreen/index.js
+++ src/components/atoms/ButtonForgetScreen/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
-import styles from './styles';
+import styles from '../ButtonsForInit/styles';
 
 export default function ButtonForgetScreen({text, onPress=null}) {
   return (
     <View>
       <TouchableOpacity
         style={{
-          ...styles.button,
+          ...styles.button1,
         }}
         onPress={onPress}>
-        <Text style={styles.text}>{text}</Text>
+        <Text style={styles.text1}>{text}</Text>
       </TouchableOpacity>
     </View>
   );
