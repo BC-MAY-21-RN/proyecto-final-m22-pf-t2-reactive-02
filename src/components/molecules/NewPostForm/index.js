68a9f88f94d74cd807c1486a2b1460bac2611b03
unknown <dag_98@hotmail.com>
2022-08-04 03:44:10 -0500

    add firebase storage
    firebase storage was added to upload the images of a post

--- src/components/molecules/NewPostForm/index.js
+++ src/components/molecules/NewPostForm/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import {View, TextInput} from 'react-native';
+import {View, TextInput, Alert} from 'react-native';
+import storage from '@react-native-firebase/storage';
+import auth from '@react-native-firebase/auth';
 import ButtonForm from '../../atoms/ButtonForm';
 import InputForm from '../../atoms/inputForm';
 import Header from '../../atoms/header';
 import styles from './styles';
 import PagerImageLocation from '../PagerImageLocation';
+
+const uploadImagesStorage = async images => {
+  const arrayUrl = [];
+
+  await Promise.all(
+    await images.map(async element => {
+      const url = element.url;
+      const nameImage = element.url.substring(element.url.lastIndexOf('/') + 1);
+      const reference = storage().ref(auth().currentUser.uid + '/' + nameImage);
+      await reference.putFile(url);
+      await reference
+        .getDownloadURL()
+        .then(urlFirebase => arrayUrl.push(urlFirebase));
+    }),
+  );
+
+  return arrayUrl;
+};
+
+const uploadPostFirestore = () => {};
+
+const uploadData = post => {
+  const {hashtags, images, location, text} = post.valuesPost;
+  if (text === '' || hashtags === '') {
+    Alert.alert('Complete los datos', '', [{text: 'ok'}]);
+  } else {
+    uploadImagesStorage(images).then(listUrl => {
+      uploadPostFirestore();
+    });
+  }
+};
+
+
 
 export default function NewPostForm({
   navigation,
@@ -36,7 +71,7 @@ export default function NewPostForm({
         post={post}
         changeVisible={changeVisible}
       />
-      <ButtonForm text={'Publicar'} onPress={() => {}} />
+      <ButtonForm text={'Publicar'} onPress={() => uploadData(post)} />
     </View>
   );
 }
