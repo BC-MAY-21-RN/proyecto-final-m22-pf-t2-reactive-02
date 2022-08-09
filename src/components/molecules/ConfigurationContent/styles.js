2ca97b0d2aa4f9d7044a5b8f1a05c477a62ec455
mrivh <m.alicia.riv@gmail.com>
2022-08-09 17:02:13 -0500

    Add: Configuration content

---
+++ src/components/molecules/ConfigurationContent/styles.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    backgroundColor: 'white',
+    borderRadius: 30,
+    padding: 20,
+  },
+  containerinfo: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+  },
+  editar: {
+    backgroundColor: '#F1F1F1',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginTop: 15,
+    alignSelf: 'center',
+  },
+});
+
+export default styles;
