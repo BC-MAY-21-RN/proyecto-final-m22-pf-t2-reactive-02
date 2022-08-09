70a7dc15ecbacd7de6bd9e0fc939fb30b04b7f6e
LizyMiGo <jmiguel@ucol.mx>
2022-08-09 16:51:27 -0500

    data sent to firebase
    form data is now sent to firebase

--- src/screens/AdoptionForm/index.js
+++ src/screens/AdoptionForm/index.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import Header from '../../components/atoms/header';
 import TopBar from '../../components/atoms/TopBar';
 import InputForm from '../../components/atoms/inputForm';
 import QuestionForm from '../../components/atoms/QuestionForm';
 import ButtonForm from '../../components/atoms/ButtonForm';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
 const question1 = ['Si', 'Aveces', 'Muy amenudo', 'Nunca'];
 const question2 = ['Casa', 'Patio', 'Calle', 'Nose'];
 
+const addForm = (
+  telefono,
+  correo,
+  ciudad,
+  mascotas,
+  horas,
+  pacifico,
+  lugar,
+) => {
+  firestore()
+    .collection('adopciones')
+    .add({
+      telefono: telefono,
+      correo: correo,
+      ciudad: ciudad,
+      mascotas: mascotas,
+      horas: horas,
+      Pacifico: pacifico,
+      lugarMascota: lugar,
+      uidUsuario: auth().currentUser.uid,
+      nombreUsuario: auth().currentUser.displayName,
+    })
+    .then(() => {});
+};
+
+const functions = ({tel, correo, ciudad, mascotas, horas, pacifico, lugar}) => {
+  addForm(tel, correo, ciudad, mascotas, horas, pacifico, lugar);
+};
+
 export default function AdoptionForm({navigation}) {
-  const handleSubmit = () => {};
+  const [tel, setTel] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [ciudad, setCiudad] = useState('');
+  const [mascotas, setMascotas] = useState('');
+  const [horas, setHoras] = useState('');
+  const [pacifico, setPacifico] = useState('');
+  const [lugar, setLugar] = useState('');
   return (
     <View>
       <TopBar navigation={navigation} iconVisible={false} />
@@ -20,26 +57,62 @@ export default function AdoptionForm({navigation}) {
         filter={true}
       />
       <View>
-        <InputForm text={'Teléfono'} />
-        <InputForm text={'Correo'} />
-        <InputForm text={'Ciudad'} />
+        <InputForm
+          text={'Teléfono'}
+          keyvalue={'telefono'}
+          change={newTel => setTel(newTel)}
+        />
+        <InputForm
+          text={'Correo'}
+          keyvalue={'correo'}
+          change={newCorreo => setCorreo(newCorreo)}
+        />
+        <InputForm
+          text={'Ciudad'}
+          keyvalue={'ciudad'}
+          change={newCiudad => setCiudad(newCiudad)}
+        />
       </View>
       <View>
-        <QuestionForm text={'¿Cuántas mascotas tienes?'} changeField={true} />
+        <QuestionForm
+          text={'¿Cuántas mascotas tienes?'}
+          changeField={true}
+          change={newMascotas => setMascotas(newMascotas)}
+        />
         <QuestionForm
           text={'¿Conviven con otras mascotas de forma pacifica?'}
           data={question1}
+          selected={selectedItem => {
+            setPacifico(selectedItem);
+          }}
         />
         <QuestionForm
           text={'¿Cuántas horas al día pasará tu mascota sola?'}
           changeField={true}
+          change={newHoras => setHoras(newHoras)}
         />
         <QuestionForm
           text={'Tu mascota estará mayormente...'}
           data={question2}
+          selected={selectedItem => {
+            setLugar(selectedItem);
+          }}
         />
       </View>
-      <ButtonForm text={'Enviar Formulario'} onPress={handleSubmit} />
+      <ButtonForm
+        text={'Enviar Formulario'}
+        onPress={() => {
+          functions({
+            tel,
+            correo,
+            ciudad,
+            mascotas,
+            horas,
+            pacifico,
+            lugar,
+          });
+        }}
+      />
     </View>
   );
 }
