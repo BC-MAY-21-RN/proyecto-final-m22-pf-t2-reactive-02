d6e2699606a20974be3c01a3fa6cec8ea635b55a
unknown <dag_98@hotmail.com>
2022-08-12 19:02:40 -0500

    fix buttonspost

--- src/components/atoms/ButtonsPost/index.js
+++ src/components/atoms/ButtonsPost/index.js
@@ -181,6 +181,76 @@ const buttonsFunction = (
   }
 };
 
+/*const onPress = index => {
+  if (index === 0) {
+    buttonsFunction(
+      like,
+      setLike,
+      data,
+      'likes',
+      getData,
+      setGetData,
+      hashtag,
+      setdisableL,
+    );
+  }
+  if (index === 3) {
+    buttonsFunction(
+      favorite,
+      setFavorite,
+      data,
+      'favoritos',
+      getData,
+      setGetData,
+      hashtag,
+      setdisableF,
+    );
+  }
+};*/
+
+const components = [
+  {
+    name: 'paw',
+    type: 'font-awesome',
+    disabled: true,
+    multicolor: true,
+    visiblecheck: false,
+    marginright: true,
+    onPress: (_, data) => {},
+  },
+  {
+    name: 'home-sharp',
+    type: 'ionicon',
+    disabled: false,
+    multicolor: false,
+    visiblecheck: true,
+    marginright: false,
+    onPress: (navigation, data) => {
+      navigation.navigate('AdoptionForm', {data: data});
+    },
+  },
+  {
+    name: 'message',
+    type: 'material-community',
+    disabled: false,
+    multicolor: false,
+    visiblecheck: false,
+    marginright: false,
+    onPress: (navigation, data) => {
+      navigation.navigate('Comments', {data: data});
+    },
+  },
+  {
+    name: 'bookmark',
+    type: 'ionicon',
+    disabled: true,
+    multicolor: true,
+    visiblecheck: false,
+    marginright: false,
+    onPress: (_, data) => {},
+  },
+];
+
 export default function ButtonsPost({
   navigation,
   data,
@@ -193,7 +263,54 @@ export default function ButtonsPost({
   const [disabledL, setdisableL] = useState(false);
   const [disabledF, setdisableF] = useState(false);
   return (
-    <View style={styles.container}>
+    <View style={styles().container}>
+      {components.map((element, i) => (
+        <Icon
+          key={i}
+          name={element.name}
+          type={element.type}
+          containerStyle={styles(element.marginright).buttons}
+          disabled={
+            element.disabled ? (i === 0 ? disabledL : disabledF) : false
+          }
+          color={
+            element.multicolor
+              ? i === 0
+                ? like
+                  ? '#6FCF97'
+                  : 'black'
+                : favorite
+                ? '#6FCF97'
+                : 'black'
+              : 'black'
+          }
+          onPress={() =>
+            element.onPress(
+              navigation,
+              data,
+              like,
+              favorite,
+              setLike,
+              setFavorite,
+              setdisableF,
+              setdisableL,
+              getData,
+              setGetData,
+              hashtag,
+            )
+          }
+        />
+      ))}
+    </View>
+  );
+}
+
+/*
+      <Icon name={'paw'} type={'font-awesome'} />
+      <Icon name={'paw'} type={'font-awesome'} />
+      <Icon name={'paw'} type={'font-awesome'} />
+      <Icon name={'paw'} type={'font-awesome'} />
+
       <Icon
         disabled={disabledL}
         name={'paw'}
@@ -246,9 +363,8 @@ export default function ButtonsPost({
           }
         />
       </View>
-    </View>
-  );
-}
+
+*/
 
 /*
         <View style={styles.separation}>
--- src/components/atoms/ButtonsPost/styles.js
+++ src/components/atoms/ButtonsPost/styles.js
@@ -1,17 +1,18 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    margin: 10,
-  },
-  buttons: {
-    flexDirection: 'row',
-  },
-  separation: {
-    marginRight: 15,
-  },
-});
+const styles = (bool, value) =>
+  StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      margin: 10,
+    },
+    buttons: {
+      margin: 10,
+      marginRight: bool ? Dimensions.get('screen').width - 200 : 0,
+    },
+    separation: {
+      marginRight: 15,
+    },
+  });
 
 export default styles;
