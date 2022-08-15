6b92cf95b6223c60ddb7e8cfd446f5a3e2312868
unknown <dag_98@hotmail.com>
2022-08-14 23:34:13 -0500

    fix duplicates in buttonspost component

--- src/components/atoms/ButtonsPost/functions.js
+++ src/components/atoms/ButtonsPost/functions.js
@@ -2,137 +2,103 @@ import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import values from '../../../const/hashtags';
 
-const foundFavorite = data => {
-  return data.favoritos[auth().currentUser.uid];
-};
-
-const foundLikes = data => {
-  return data.likes[auth().currentUser.uid];
-};
-
-const foundAdoption = data => {
-  const hashtags = data.hashtags;
-  let contador = 0;
-  values.adoption.map(element => {
-    if (hashtags.indexOf(element) >= 0) {
-      contador++;
-    }
-  });
-  return contador;
-};
-
-const updateLike = (id, likes, functionType, data, disablefunction) => {
-  firestore()
-    .collection('posts')
+const updateFunction = (collection, id, key, data) => {
+  return firestore()
+    .collection(collection)
     .doc(id)
-    .update({likes: likes})
-    .then(() => {
-      firestore()
-        .collection('favoritos')
-        .doc(id + auth().currentUser.uid)
-        .update({['post.likes']: likes})
-        .then(() => {
-          if (functionType === 'put') {
-            addLike(data, disablefunction);
-          } else {
-            deleteLike(data, disablefunction);
-          }
-        })
-        .catch(() => {
-          if (functionType === 'put') {
-            addLike(data, disablefunction);
-          } else {
-            deleteLike(data, disablefunction);
-          }
-        });
-    });
+    .update({[key]: data});
 };
 
-const addLike = (data, disablefunction) => {
+const setFunction = (collection, id, data, disablefunction) => {
   firestore()
-    .collection('likes')
-    .doc(data.idDoc + auth().currentUser.uid)
-    .set({
+    .collection(collection)
+    .doc(id)
+    .set(data)
+    .then(() => disablefunction(false))
+    .catch(() => disablefunction(false));
+};
+
+const deleteFunction = (id, disablefunction, collection) => {
+  firestore()
+    .collection(collection)
+    .doc(id)
+    .delete()
+    .then(() => disablefunction(false))
+    .catch(() => disablefunction(false));
+};
+
+const updateFavorite = (id, newValues, functionType, data, disablefunction) => {
+  updateFunction('posts', id, 'favoritos', newValues)
+    .then(() => {
+      const newId = id + auth().currentUser.uid;
+      if (functionType === 'put') {
+        const newData = {...data, favoritos: {[auth().currentUser.uid]: true}};
+        delete newData.idDoc;
+        const dataJSON = {
+          uidUsuario: auth().currentUser.uid,
+          idPost: id,
+          post: newData,
+          fecha: firestore.Timestamp.fromMillis(Date.now()),
+        };
+        setFunction('favoritos', newId, dataJSON, disablefunction);
+      } else {
+        deleteFunction(newId, disablefunction, 'favoritos');
+      }
+    })
+    .catch(() => {});
+};
+
+const typefunctionLike = (functionType, data, disablefunction) => {
+  const id = data.idDoc + auth().currentUser.uid;
+  if (functionType === 'put') {
+    const dataJSON = {
       uidUsuario: auth().currentUser.uid,
       idPost: data.idDoc,
       usuarioOP: data.uidUsuario,
       fecha: firestore.Timestamp.fromMillis(Date.now()),
-    })
-    .then(() => disablefunction(false))
-    .catch(() => disablefunction(false));
-};
-
-const deleteLike = (data, disablefunction) => {
-  firestore()
-    .collection('likes')
-    .doc(data.idDoc + auth().currentUser.uid)
-    .delete()
-    .then(() => disablefunction(false))
-    .catch(() => disablefunction(false));
-};
-
-const addFavorite = (id, data, disablefunction) => {
-  const newData = {...data, favoritos: {[auth().currentUser.uid]: true}};
-  delete newData.idDoc;
-  firestore()
-    .collection('favoritos')
-    .doc(id + auth().currentUser.uid)
-    .set({
-      uidUsuario: auth().currentUser.uid,
-      idPost: id,
-      post: newData,
-      fecha: firestore.Timestamp.fromMillis(Date.now()),
-    })
-    .then(_ => disablefunction(false))
-    .catch(error => disablefunction(false));
-};
-
-const deleteFavorite = (id, disablefunction) => {
-  firestore()
-    .collection('favoritos')
-    .doc(id + auth().currentUser.uid)
-    .delete()
-    .then(() => disablefunction(false))
-    .catch(() => disablefunction(false));
-};
-
-const updateFavorite = (id, favorites, functionType, data, disablefunction) => {
-  firestore()
-    .collection('posts')
-    .doc(id)
-    .update({favoritos: favorites})
-    .then(() => {
-      if (functionType === 'put') {
-        addFavorite(id, data, disablefunction);
-      } else {
-        firestore()
-          .collection('favoritos')
-          .doc(id + auth().currentUser.uid)
-          .update({['post.favoritos']: favorites})
-          .then(() => {
-            deleteFavorite(id, disablefunction);
-          });
-      }
-    });
-};
-
-const filterType = (data, type, disablefunction, obj) => {
-  if (type === 'likes') {
-    updateLike(data.idDoc, obj, 'put', data, disablefunction);
+    };
+    setFunction('likes', id, dataJSON, disablefunction);
   } else {
-    updateFavorite(data.idDoc, obj, 'put', data, disablefunction);
+    deleteFunction(id, disablefunction, 'likes');
+  }
+};
+
+const updateLike = (id, newValues, functionType, data, disablefunction) => {
+  updateFunction('posts', id, 'likes', newValues)
+    .then(() => {
+      updateFunction(
+        'favoritos',
+        id + auth().currentUser.uid,
+        'post.likes',
+        newValues,
+      )
+        .then(() => {
+          typefunctionLike(functionType, data, disablefunction);
+        })
+        .catch(() => {
+          typefunctionLike(functionType, data, disablefunction);
+        });
+    })
+    .catch(() => {});
+};
+
+const filterType = (data, type, disablefunction, obj, functionType) => {
+  if (type === 'likes') {
+    updateLike(data.idDoc, obj, functionType, data, disablefunction);
+  } else {
+    updateFavorite(data.idDoc, obj, functionType, data, disablefunction);
   }
 };
 
 const put = (data, type, disablefunction) => {
   const obj = {...data[type], ...{[auth().currentUser.uid]: true}};
-  filterType(data, type, disablefunction, obj);
+  filterType(data, type, disablefunction, obj, 'put');
 };
 
 const remove = (data, type, disablefunction) => {
   const obj = data[type];
   delete obj[auth().currentUser.uid];
-  filterType(data, type, disablefunction, obj);
+  filterType(data, type, disablefunction, obj, 'remove');
 };
 
 const deleteComponent = (getData, setGetData, hashtag, data, type) => {
@@ -153,8 +119,8 @@ const buttonsFunction = (
   hashtag,
   disablefunction,
 ) => {
+  disablefunction(true);
   if (value) {
-    disablefunction(true);
     remove(data, type, disablefunction);
     deleteComponent(getData, setGetData, hashtag, data, type);
     const hashtags = hashtag.toString().replace(/ /g, '').split('#');
@@ -162,10 +128,28 @@ const buttonsFunction = (
       setValue(!value);
     }
   } else {
-    disablefunction(true);
     put(data, type, disablefunction);
     setValue(!value);
   }
+};
+
+const foundFavorite = data => {
+  return data.favoritos[auth().currentUser.uid];
+};
+
+const foundLikes = data => {
+  return data.likes[auth().currentUser.uid];
+};
+
+const foundAdoption = data => {
+  const hashtags = data.hashtags;
+  let contador = 0;
+  values.adoption.map(element => {
+    if (hashtags.indexOf(element) >= 0) {
+      contador++;
+    }
+  });
+  return contador;
 };
 
 export default {buttonsFunction, foundFavorite, foundLikes, foundAdoption};
