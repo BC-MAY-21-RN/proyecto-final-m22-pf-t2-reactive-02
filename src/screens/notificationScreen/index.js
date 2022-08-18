43dae647c763677678bbc3abc42fbdbd0ee4446e
LizyMiGo <jmiguel@ucol.mx>
2022-08-17 21:46:21 -0500

    adoption info filtered
    The info of the notifications for the current user was filtered

--- src/screens/notificationScreen/index.js
+++ src/screens/notificationScreen/index.js
@@ -1,25 +1,81 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+  Pressable,
+} from 'react-native';
+import NotificationAdoption from '../../components/atoms/NotificationAdoption';
+import Header from '../../components/atoms/header';
+import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
-import NotificationComponent from '../../components/atoms/NotificationComponent';
+import styles from './styles';
+import AdoptionInfo from '../../components/atoms/AdoptionInfo';
 
-const logout = () => {
-  auth()
-    .signOut()
-    .then(() => {});
-};
+function info(changeGetData) {
+  const id = auth().currentUser.uid;
+  firestore()
+    .collection('adopciones')
+    .where('uidPosteo', '==', id)
+    .get()
+    .then(querySnapshot => {
+      var data = [];
+      querySnapshot.forEach(documentSnapshot => {
+        data.push({
+          ...documentSnapshot.data(),
+          idDoc: documentSnapshot.ref.id,
+        });
+        changeGetData(data);
+      });
+    });
+}
 
-export default function NotificationScreen() {
+export default function NotificationScreen({navigation, data}) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [getData, setGetData] = useState([]);
+  const [infoModal, setInfoModal] = useState([]);
+  const changeGetData = adoptions => setGetData(adoptions);
+  useEffect(() => {
+    info(changeGetData);
+  }, []);
   return (
     <View>
-      <NotificationComponent />
-      <Text>Notification</Text>
-      <TouchableOpacity
-        onPress={() => {
-          logout();
+      <Header text={'Notificaciones'} navigation={navigation} />
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
         }}>
-        <Text>Salir</Text>
-      </TouchableOpacity>
+        <View style={styles.info}>
+          <AdoptionInfo data={infoModal} />
+          <Pressable
+            style={styles.close}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <Text>Cerrar</Text>
+          </Pressable>
+        </View>
+      </Modal>
+      <FlatList
+        data={getData}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            style={styles.card}
+            onPress={() => {
+              setInfoModal(item);
+              setModalVisible(true);
+            }}>
+            <NotificationAdoption
+              data={item}
+              setGetData={setGetData}
+              getData={getData}
+            />
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
---
+++ src/screens/notificationScreen/styles.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    width: '95%',
+    alignSelf: 'center',
+    marginTop: 7,
+    padding: 10,
+    borderRadius: 6,
+  },
+  info: {
+    backgroundColor: '#E1E1E1',
+    width: '85%',
+    height: '70%',
+    elevation: 50,
+    alignSelf: 'center',
+    top: '10%',
+    padding: 20,
+    borderRadius: 20,
+  },
+  close: {
+    backgroundColor: '#7B9CB2',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    margin: 30,
+    alignSelf: 'center',
+    borderRadius: 10,
+    top: '30%',
+  },
+});
+
+export default styles;
