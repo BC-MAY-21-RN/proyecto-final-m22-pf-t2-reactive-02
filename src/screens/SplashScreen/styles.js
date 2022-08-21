72f815d2d8e5d933e45835299805dd6d98068cf3
unknown <dag_98@hotmail.com>
2022-08-20 19:29:41 -0500

    fix splashscreen

---
+++ src/screens/SplashScreen/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  containerName: {
+    alignItems: 'center',
+    marginVertical: 200,
+  },
+});
+
+export default styles;
